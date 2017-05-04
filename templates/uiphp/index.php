ae251b892182c4d40958f9dd897751cb815b20c5
dm <polowesa>
2017-05-04 17:55:12 +0300

    edit index.php tamplate

--- templates/uiphp/index.php
+++ templates/uiphp/index.php
@@ -20,33 +20,33 @@ $doc->setHtml5(true);
 
 $menu   = $app->getMenu();
 $active = $menu->getActive();
-
 $class  = $active->alias . " pageid-" . $active->id;
-// Getting params from template
-$params = $app->getTemplate(true)->params;
 
-// Detecting Active Variables
-$option   = $app->input->getCmd('option', '');
-$view     = $app->input->getCmd('view', '');
-$layout   = $app->input->getCmd('layout', '');
-$task     = $app->input->getCmd('task', '');
-$itemid   = $app->input->getCmd('Itemid', '');
-$sitename = $app->get('sitename');
+//// Getting params from template
+//$params = $app->getTemplate(true)->params;
+//
+//// Detecting Active Variables
+//$option   = $app->input->getCmd('option', '');
+//$view     = $app->input->getCmd('view', '');
+//$layout   = $app->input->getCmd('layout', '');
+//$task     = $app->input->getCmd('task', '');
+//$itemid   = $app->input->getCmd('Itemid', '');
+//$sitename = $app->get('sitename');
 
 
-
+/*
 // Add JavaScript Frameworks
 JHtml::_('bootstrap.framework');
 
 $doc->addScriptVersion($this->baseurl . '/templates/' . $this->template . '/js/common.js');
 
 // Add Stylesheets
-$doc->addStyleSheetVersion($this->baseurl . '/templates/' . $this->template . '/css/style.css');
+$doc->addStyleSheetVersion($this->baseurl . '/templates/' . $this->template . '/css/style.css');*/
 
 
 ?>
 <!DOCTYPE html>
-<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<html lang="<?php echo $this->language; ?>">
 
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -54,16 +54,9 @@ $doc->addStyleSheetVersion($this->baseurl . '/templates/' . $this->template . '/
 	<!--[if lt IE 9]><script src="< ?php echo JUri::root(true); ?>/media/jui/js/html5.js"></script><![endif]-->
 </head>
 
-<body class="site <?php echo $option
-	. ' view-' . $view
-	. ($layout ? ' layout-' . $layout : ' no-layout')
-	. ($task ? ' task-' . $task : ' no-task')
-	. ($itemid ? ' itemid-' . $itemid : '')
-	. ($params->get('fluidContainer') ? ' fluid' : '');
-	echo ($this->direction == 'rtl' ? ' rtl' : '');
-?>">
+<body class="body <?php echo $class; ?>">
 	<!-- Body -->
-	<div class="body">
+	<div class="site">
 
 			<!-- Header -->
 			<header>
@@ -72,9 +65,12 @@ $doc->addStyleSheetVersion($this->baseurl . '/templates/' . $this->template . '/
 
 			</header>
 
-				<!-- Begin Content -->
-				<jdoc:include type="modules" name="position_2" style="xhtml" />
-				<!-- End Content -->
+			<!-- Begin Content -->
+
+			<jdoc:include type="modules" name="content" style="xhtml" />
+			<jdoc:include type="component" />
+
+			<!-- End Content -->
 
 
 
