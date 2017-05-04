d081c718d3f3a74461165ed83a5410e75a071ff7
dm <polowesa>
2017-05-04 17:43:38 +0300

    install joomla template

---
+++ templates/uiphp/index.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.UIPHP
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$app             = JFactory::getApplication();
+$doc             = JFactory::getDocument();
+$user            = JFactory::getUser();
+$this->language  = $doc->language;
+$this->direction = $doc->direction;
+
+// Output as HTML5
+$doc->setHtml5(true);
+
+$menu   = $app->getMenu();
+$active = $menu->getActive();
+
+$class  = $active->alias . " pageid-" . $active->id;
+// Getting params from template
+$params = $app->getTemplate(true)->params;
+
+// Detecting Active Variables
+$option   = $app->input->getCmd('option', '');
+$view     = $app->input->getCmd('view', '');
+$layout   = $app->input->getCmd('layout', '');
+$task     = $app->input->getCmd('task', '');
+$itemid   = $app->input->getCmd('Itemid', '');
+$sitename = $app->get('sitename');
+
+
+
+// Add JavaScript Frameworks
+JHtml::_('bootstrap.framework');
+
+$doc->addScriptVersion($this->baseurl . '/templates/' . $this->template . '/js/common.js');
+
+// Add Stylesheets
+$doc->addStyleSheetVersion($this->baseurl . '/templates/' . $this->template . '/css/style.css');
+
+
+?>
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<jdoc:include type="head" />
+	<!--[if lt IE 9]><script src="< ?php echo JUri::root(true); ?>/media/jui/js/html5.js"></script><![endif]-->
+</head>
+
+<body class="site <?php echo $option
+	. ' view-' . $view
+	. ($layout ? ' layout-' . $layout : ' no-layout')
+	. ($task ? ' task-' . $task : ' no-task')
+	. ($itemid ? ' itemid-' . $itemid : '')
+	. ($params->get('fluidContainer') ? ' fluid' : '');
+	echo ($this->direction == 'rtl' ? ' rtl' : '');
+?>">
+	<!-- Body -->
+	<div class="body">
+
+			<!-- Header -->
+			<header>
+
+				<jdoc:include type="modules" name="header" style="xhtml" />
+
+			</header>
+
+				<!-- Begin Content -->
+				<jdoc:include type="modules" name="position_2" style="xhtml" />
+				<!-- End Content -->
+
+
+
+	</div>
+	<!-- Footer -->
+	<footer>
+		<jdoc:include type="modules" name="footer" style="xhtml" />
+	</footer>
+
+</body>
+</html>
